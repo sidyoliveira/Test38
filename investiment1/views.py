889607f75721f2498b65890de99969bb43bb52fc
Sidney dos Santos Oliveira <sidyoliveira2005@gmail.com>
2025-06-20 18:27:21 -0300

    creation of the HTML head and bottom geral

--- investiment1/views.py
+++ investiment1/views.py
@@ -1,15 +1,15 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def my_portfolio(request):
-    return render(request, "investment1/my_portfolio.html")
+    return render(request, "investment1/pages/my_portfolio.html")
 
 
 def my_status(request):
-    return render(request, "investment1/my_status.html")
+    return render(request, "investment1/pages/my_status.html")
 
 
 def my_newspaper(request):
-    return render(request, "investment1/my_newspaper.html")
+    return render(request, "investment1/pages/my_newspaper.html")
 
